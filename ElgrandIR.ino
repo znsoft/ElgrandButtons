9f9621e38f70bed137bb87b4b4c7c3091350ab1f
znsoft <zn@bk.ru>
2016-09-29 00:44:26 +1000

    не заработало, отлаживаю

--- ElgrandIR.ino
+++ ElgrandIR.ino
@@ -1,36 +1,14 @@
-/*
-   IRrecord: record and play back IR signals as a minimal
-   An IR detector/demodulator must be connected to the input RECV_PIN.
-   An IR LED must be connected to the output PWM pin 3.
-   A button must be connected to the input BUTTON_PIN; this is the
-   send button.
-   A visible LED can be connected to STATUS_PIN to provide status.
-
-   The logic is:
-   If the button is pressed, send the IR code.
-   If an IR code is received, record it.
-
-   Version 0.11 September, 2009
-   Copyright 2009 Ken Shirriff
-   http://arcfn.com
-
-
-*/
 
 #include <IRremote.h>
-
+int RECV_PIN = A5;
 #include "UniversalIR.h"
 
-int RECV_PIN = A5;
+
 int BUTTON_PIN = 9;
 int STATUS_PIN = 13;
 int SPEAKER_PIN = 4;
-//irsend = 3
-
-IRrecv irrecv(RECV_PIN);
-
-
-decode_results results;
+int buttonspin[4] = {8, 7, 5, 2};
+MyMenu myMenu(BUTTON_PIN, SPEAKER_PIN, buttonspin);
 
 
 
@@ -38,75 +16,13 @@ void setup()
 {
 
 
-  Serial.begin(9600);
+ 
 
-  irrecv.enableIRIn(); // Start the receiver
-  pinMode(3, OUTPUT);
-  pinMode(BUTTON_PIN, INPUT);
-  digitalWrite(BUTTON_PIN, HIGH);       // turn on pullup resistors
-  pinMode(STATUS_PIN, OUTPUT);
+  //myMenu = new MyMenu
 
 }
 
-// Storage for the recorded code
-//int codeType = -1; // The type of code
-//unsigned long codeValue; // The code value if not raw
-unsigned int rawCodes[RAWBUF]; // The durations if raw
-int codeLen; // The length of the code
-//int toggle = 0; // The RC5/6 toggle state
-
-
-// Stores the code for later playback
-// Most of this code is just logging
-void storeCode(decode_results *results) {
-  //codeType = results->decode_type;
-  //int count = results->rawlen;
-  // if (codeType == UNKNOWN) {
-  Serial.println("Received code, saving ");
-  codeLen = results->rawlen - 1;
-  // To store raw codes:
-  // Drop first value (gap)
-  // Convert from ticks to microseconds
-  // Tweak marks shorter, and spaces longer to cancel out IR receiver distortion
-  for (int i = 1; i <= codeLen; i++) {
-    if (i % 2) {
-      // Mark
-      rawCodes[i - 1] = results->rawbuf[i] * USECPERTICK - MARK_EXCESS;
-      Serial.print(" m");
-    }
-    else {
-      // Space
-      rawCodes[i - 1] = results->rawbuf[i] * USECPERTICK + MARK_EXCESS;
-      Serial.print(" s");
-    }
-    Serial.print(rawCodes[i - 1], DEC);
-  }
-  Serial.println("");
-
-}
-
-
-
-int lastButtonState;
 
 void loop() {
-  // If button pressed, send the code.
-  int buttonState = !digitalRead(BUTTON_PIN);
-
-  if (buttonState) {
-    Serial.println("Pressed, sending");
-    digitalWrite(STATUS_PIN, HIGH);
-
-    sendCode();
-    digitalWrite(STATUS_PIN, LOW);
-    delay(150); // Wait a bit between retransmissions
-  }
-  else if (irrecv.decode(&results)) {
-    irrecv.enableIRIn(); // Re-enable receiver
-    digitalWrite(STATUS_PIN, HIGH);
-    storeCode(&results);
-    irrecv.resume(); // resume receiver
-    digitalWrite(STATUS_PIN, LOW);
-  }
-  lastButtonState = buttonState;
+  myMenu.ProcessButtons();
 }
--- UniversalIR.h
+++ UniversalIR.h
@@ -1,9 +1,11 @@
 
 #include <EEPROM.h>
 #include <IRremote.h>
-#include <HashMap.h>
-IRsend irsend;
 
+IRsend irsend; //irsend = 3 pin on arduino nano
+IRrecv irrecv(RECV_PIN);
+
+decode_results results;
 // bad
 #define ARRAY_SIZE(array) (sizeof((array))/sizeof((array[0])))
 
@@ -16,21 +18,65 @@ template <unsigned S> inline unsigned arraysize(int (&v)[S]) {
 enum MyMode {
   Config,
   Play
-
 };
 
 
 class MyMenu {
   private:
-    int usePins[];
+    int *usePins;
     int speakerpin;
     MyMode myMode;
-    int myFat[];
+    unsigned int *myFat;
     int configButtonPin;
     int learnCount, nowLearnPin;
     int buttonsCount;
+    //tempcode
+    int *rawCodes;//[,];//RAWBUF]; // The durations
+    int codeLen; // The length of the code
 
-    //Buzzer
+    // Stores the code for later playback
+    // Most of this code is just logging
+    void storeCode(decode_results *results, unsigned int index) {
+      int j;
+      for (j = 0; j < buttonsCount; j++)
+        if (index == usePins[j])break;
+
+
+      codeLen = results->rawlen - 1;
+      // To store raw codes:
+      // Drop first value (gap)
+      // Convert from ticks to microseconds
+      // Tweak marks shorter, and spaces longer to cancel out IR receiver distortion
+      for (int i = 1; i <= codeLen; i++) {
+        if (i % 2) {
+          // Mark
+          rawCodes[j, i ] = results->rawbuf[i] * USECPERTICK - MARK_EXCESS;
+        }
+        else {
+          // Space
+          rawCodes[j, i ] = results->rawbuf[i] * USECPERTICK + MARK_EXCESS;
+        }
+      }
+      rawCodes[j, 0 ] = codeLen;
+    }
+
+
+    void SaveMyEEPROM() {
+      myFat[0] = sizeof(myFat);
+      for (int i = 1; i < buttonsCount; i++)  myFat[i] = myFat[i - 1] + rawCodes[i - 1, 0 ];
+      EEPROM.put(0, myFat);
+      for (int i = 0; i < buttonsCount; i++)
+        for (int j = 0; j < rawCodes[i, 0]; j++)
+          EEPROM.write(myFat[i] + j,  rawCodes[i, j]);
+    }
+
+
+
+    void ReadMyEEPROM() {
+      EEPROM.get(0, myFat);
+    }
+
+    //Buzzer for arduino nano , there is no tone method on it
     void buzz( long frequency, long length) {
       long delayValue = 1000000 / frequency / 2; // calculate the delay value between transitions
       //// 1 second's worth of microseconds, divided by the frequency, then split in half since
@@ -55,63 +101,49 @@ class MyMenu {
     }
 
 
-  public:
-
-    //Constructor
-    template <unsigned S>
-    MyMenu(int configPin, int speaker, int (&buttonsPins)[S]) {
-      speakerpin = speaker;
-      buttonsCount = S ;
-      usePins = buttonsPins;
-      configButtonPin = configPin;
-      myMode = Play;
-      myFat = new int[S];
-      InitAllPinsIn(buttonsPins);
-      ReadMyEEPROM();
-      //if (IsMyEEPROMEmpty(S))        myMode = Config;
-      pinMode(configButtonPin, INPUT);
-      digitalWrite(configButtonPin, HIGH);
-      melody1();
-    }
-
-
     void ProcessSendButtons() {
-      //usePins
-      int S = ARRAY_SIZE(usePins);
-
-      //int S = ARRAY_SIZE(myFat);
-      for (int i = 0; i < S; i++) {
+      int countPressedButtons = 0;
+      for (int i = 0; i < buttonsCount; i++) {
         int buttonPin = usePins[i];
         if (!digitalRead(buttonPin)) {
-          unsigned int adr = myFat[i];
-          unsigned int len = myFat[i + 1] - adr;
-          int *code = new unsigned int[len];
+          Serial.println("press");
+
+          countPressedButtons++;
+          unsigned int adr = myFat[i] >> 8;
+          unsigned int len = (myFat[i] & 0xff) - adr;
+          unsigned int *code = new unsigned int[len];
           EEPROM.get(adr, code);
           irsend.sendRaw(code, len, 38);
+           Serial.print(buttonPin);
+           Serial.println(" send");
         }
       }
+      //if (countPressedButtons == buttonsCount)
+
       if (!digitalRead(configButtonPin)) {
         myMode = Config;
+        Serial.println("Config");
+
         learnCount = 0, nowLearnPin = 0;
+        memset(rawCodes, 0 , sizeof(rawCodes));
         melody2();
-      }
-    }
-
-
-    void ProcessButtons() {
-      if (myMode == Config) {
-        LearnButtons();
-      } else {
-        ProcessSendButtons();
+        irrecv.enableIRIn(); // Start the receiver
       }
     }
 
 
     void  LearnButtons() {
-      int S = ARRAY_SIZE(usePins);
-      for (int i = 0; i < S; i++) {
+      for (int i = 0; i < buttonsCount; i++) {
         int buttonPin = usePins[i];
         if (!digitalRead(buttonPin)) nowLearnPin = buttonPin;
+      }
+      if (nowLearnPin == 0 )return;
+      if (irrecv.decode(&results)) {
+        irrecv.enableIRIn(); // Re-enable receiver
+        storeCode(&results, nowLearnPin);
+        SaveMyEEPROM();
+        irrecv.resume(); // resume receiver
+        melody3();
       }
     }
 
@@ -125,19 +157,61 @@ class MyMenu {
 
     void melody2() {
       buzz( 2000, 500);
-      buzz( 1, 500);
-      buzz( 1500, 500);
-      buzz( 3, 500);
+      buzz( 5, 400);
+      buzz( 1500, 400);
+      buzz( 5, 400);
       buzz( 2000, 500);
     }
 
-
-    void ReadMyEEPROM() {
-
-      EEPROM.get(0, myFat);
-
+    void melody3() {
+      buzz( 3000, 300);
+      buzz( 1500, 300);
     }
 
+  public:
+
+    //Constructor
+    template <unsigned S>
+    MyMenu(int configPin, int speaker, int (&buttonsPins)[S]) {
+      speakerpin = speaker;
+      buttonsCount = S ;
+      Serial.begin(9600);
+      Serial.println(buttonsCount);
+      usePins = buttonsPins;
+      configButtonPin = configPin;
+      myMode = Play;
+      rawCodes = new int[S, RAWBUF];
+      memset(rawCodes, 0 , sizeof(rawCodes));
+      myFat = new unsigned int[S];
+      InitAllPinsIn(buttonsPins);
+      ReadMyEEPROM();
+      //if (IsMyEEPROMEmpty(S))        myMode = Config;
+      pinMode(configButtonPin, INPUT);
+      digitalWrite(configButtonPin, HIGH);
+      melody1();
+      // irrecv = new IRrecv(recvPin);
+    }
+
+
+
+
+
+    void ProcessButtons() {
+      if (myMode == Config) {
+        LearnButtons();
+      } else {
+        ProcessSendButtons();
+      }
+    }
+
+
+
+
 };
+
+
+
+
+
 
 
